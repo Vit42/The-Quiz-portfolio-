9558ae67b0df6beb6c3def2560d97a033e0a71c8
Vit 42 <v.v.safronov@gmail.com>
2021-01-26 18:28:34 +0100

    Initial Commit

---
+++ The Quiz (portfolio)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Quiz (portfolio)
+//
+//  Created by Vitalii Safronov on 2021. 01. 26..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
